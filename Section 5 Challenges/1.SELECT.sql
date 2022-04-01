84f194ed921c8f16263d45f1fe5b0e5091cf62bf
Mostafa Hosseini <kavrin912@gmail.com>
2022-04-01 17:32:34 +0000

    add section 5 (CRUD)

---
+++ Section 5 Challenges/1.SELECT.sql
@@ -0,0 +1,7 @@
+SELECT cat_id FROM cats;
+
+SELECT name, breed FROM cats;
+
+SELECT name, age FROM cats WHERE breed = 'Tabby';
+
+SELECT cat_id, age FROM cats WHERE cat_id = age;
---
+++ Section 5 Challenges/2.UPDATE.sql
@@ -0,0 +1,5 @@
+UPDATE cats SET name = 'Jack' WHERE name = 'Jackson';
+
+UPDATE cats SET breed = 'British Shorthair' WHERE name = 'Ringo';
+
+UPDATE cats SET age = 12 WHERE breed = 'Maine Coon';
